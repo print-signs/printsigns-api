abecdce4b7a4e974322e1085c3a5f52f138f5984
pawan-dot <71133473+pawan-dot@users.noreply.github.com>
2022-06-21 21:36:16 +0530

    make it betterlkjkfk

--- routes/OffersRoute.js
+++ routes/OffersRoute.js
@@ -9,9 +9,9 @@ import {
 const router = express.Router();
 import { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js"
 
-router.route("/offer/create/").post(isAuthenticatedUser, authorizeRoles("admin"), createOffer)
+router.route("/offer/create/").post(createOffer)//isAuthenticatedUser, authorizeRoles("admin"),
 router.route("/offer/getAll/").get(getAllOffer)
 router.route("/offer/getOne/:id").get(getOneOffer)
-router.route("/offer/update/:id").put(isAuthenticatedUser, authorizeRoles("admin"), updateOffer);
-router.route("/offer/delete/:id").delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOffer);
+router.route("/offer/update/:id").put(updateOffer);//isAuthenticatedUser, authorizeRoles("admin"),
+router.route("/offer/delete/:id").delete(deleteOffer);//isAuthenticatedUser, authorizeRoles("admin"),
 export default router;
