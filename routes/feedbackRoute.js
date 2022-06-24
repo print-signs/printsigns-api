2fa0bb4dec88ebe5bd799bee2da3b11d70c4faf6
pawan-dot <71133473+pawan-dot@users.noreply.github.com>
2022-06-24 13:03:35 +0530

    make itxc

--- routes/feedbackRoute.js
+++ routes/feedbackRoute.js
@@ -9,5 +9,5 @@ const router = express.Router();
 import { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js"
 router.route("/feedback/create/").post(isAuthenticatedUser, createFeedback)
 router.route("/feedback/getAll/").get(isAuthenticatedUser, authorizeRoles('admin'), getAllFeedback)
-router.route("/feedback/getOne/").get(isAuthenticatedUser, authorizeRoles('admin'), getAOneFeedback)
+router.route("/feedback/getOne/:id").get(isAuthenticatedUser, authorizeRoles('admin'), getAOneFeedback)
 export default router;  
