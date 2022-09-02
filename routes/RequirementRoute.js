ba9285593f99f48bbed3e4cce77eca7903d30df6
pawan-dot <71133473+pawan-dot@users.noreply.github.com>
2022-09-02 11:48:30 +0530

    auth

--- routes/RequirementRoute.js
+++ routes/RequirementRoute.js
@@ -14,7 +14,7 @@ import { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js"
 const router = express.Router();
 
 router.route("/requirement/create/").post(isAuthenticatedUser, createRequirement)
-router.route("/requirement/getAll/").get(getAllRequirement)
+router.route("/requirement/getAll/").get(isAuthenticatedUser, getAllRequirement)
 router.route("/requirement/getOne/:id").get(isAuthenticatedUser, getOneRequirement)
 router.route("/requirement/update/:id").put(isAuthenticatedUser, updateRequirement);
 router.route("/requirement/delete/:id").delete(isAuthenticatedUser, deleteOneRequirement);
