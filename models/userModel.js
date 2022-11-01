53c55e27225bd1dc2fdac20928b778be253f385a
pawan-dot <71133473+pawan-dot@users.noreply.github.com>
2022-11-01 15:38:36 +0530

    cms

--- models/userModel.js
+++ models/userModel.js
@@ -64,9 +64,7 @@ userSchema.pre("save", async function (next) {
 
 // JWT TOKEN
 userSchema.methods.getJWTToken = function () {
-    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {//make token
-
-    });
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET);
 };
 // console.log(process.env.JWT_SECRET)
 
